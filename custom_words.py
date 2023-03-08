7f5071cb9bbafbba6651121f8df57e755ebd0d29
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-07 19:24:21 -0500

    Update custom_words.py

--- custom_words.py
+++ custom_words.py
@@ -1,24 +1,28 @@
-def custom_task():
-    # Your custom task code here
-    pass
+import random
 
-def custom_script():
-    # Your custom script code here
-    pass
+def handle_custom_words(text):
+    if text.lower() == "tell me a joke":
+        # Define a list of jokes
+        jokes = [
+            "Why don't scientists trust atoms? Because they make up everything!",
+            "Why did the scarecrow win an award? Because he was outstanding in his field!",
+            "Why did the tomato turn red? Because it saw the salad dressing!",
+            "Why don't skeletons fight each other? They don't have the guts!",
+            "Why did the chicken cross the playground? To get to the other slide!"
+        ]
+        
+        # Select a random joke from the list
+        joke = random.choice(jokes)
 
-def custom_task():
-    # Your custom task code here
-    pass
+        # Print the joke
+        print(joke)
 
-def custom_script():
-    # Your custom script code here
-    pass
 
-def custom_task():
-    # Your custom task code here
-    pass
+#def custom_task():
+#    # Your custom task code here
+#    pass
 
-def custom_script():
-    # Your custom script code here
-    pass
-# Add more custom words/functions as needed
+#def custom_script():
+#    # Your custom script code here
+#    pass
+
