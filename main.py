ab120edfe39b634b4c0e470742a8fced462fd649
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-07 19:19:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,22 +17,23 @@ def handle_user_message(update, context):
     custom_words.handle_custom_words(text)
 
     # Check if the user wants to use the Google Assistant API
-    if text.startswith("ask google"):
+    if text.startswith("OpenAI"):
         # Remove the "ask google" prefix from the message text
-        text = text.replace("ask google", "", 1).strip()
+        text = text.replace("OpenAI", "", 1).strip()
 
-        # Generate a response using the Google Assistant API
-        response = get_assistant_response(text)
-
-        # Send the Google Assistant response back to the user
-        update.message.reply_text(response)
-    else:
         # The user does not want to use the Google Assistant API, generate a response using GPT-3
         response = get_chatbot_response(text)
 
         # Send the chatbot response back to the user
         update.message.reply_text(response)
+    else:
+        # Generate a response using the Google Assistant API
+        response = get_assistant_response(text)
 
+        # Send the Google Assistant response back to the user
+        update.message.reply_text(response)
+        
+        
 # Set up Telegram bot and start polling for user messages
 updater = Updater(bot_token, use_context=True)
 dispatcher = updater.dispatcher
