1a6a219999dc5b8cd8e65d56463402204017c468
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-09 20:09:05 -0500

    Update custom_words.py

--- custom_words.py
+++ custom_words.py
@@ -1,21 +1,23 @@
 import random
 
-def handle_custom_words(text):
-    if text.lower() == "tell me a joke":
-        # Define a list of jokes
-        jokes = [
-            "Why don't scientists trust atoms? Because they make up everything!",
-            "Why did the scarecrow win an award? Because he was outstanding in his field!",
-            "Why did the tomato turn red? Because it saw the salad dressing!",
-            "Why don't skeletons fight each other? They don't have the guts!",
-            "Why did the chicken cross the playground? To get to the other slide!"
-        ]
-        
-        # Select a random joke from the list
-        joke = random.choice(jokes)
+words = {
+    "tell me a joke": [
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "Why did the scarecrow win an award? Because he was outstanding in his field!",
+        "Why did the tomato turn red? Because it saw the salad dressing!",
+        "Why don't skeletons fight each other? They don't have the guts!",
+        "Why did the chicken cross the playground? To get to the other slide!",
+    ]
+}
 
-        # Print the joke
-        print(joke)
+
+def handle_custom_words(text):
+    if text.lower() in words:
+        # Select a random response from the list of responses
+        response = random.choice(words[text.lower()])
+        print(response)
+        return True
+    return False
 
 
 #def custom_task():
